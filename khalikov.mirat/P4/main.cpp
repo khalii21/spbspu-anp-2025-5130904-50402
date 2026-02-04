510e2f65b220856ded9028d27db0ef907dc3dbc7
khalii21 <miratkhalikov007@gmail.com>
2026-02-04 22:58:03 +0500

    excsnd func is added

--- khalikov.mirat/P4/main.cpp
+++ khalikov.mirat/P4/main.cpp
@@ -32,7 +32,7 @@ char * getline(std::istream & in, size_t & s)
   char ch = 0;
   size_t i = 0;
   size_t capacity = 2;
-  char * result = (char*)malloc(capacity * sizeof(char));
+  char * result = reinterpret_cast< char * >(malloc(sizeof(char) * capacity));;
   if (result == nullptr)
   {
     throw std::bad_alloc();
@@ -54,8 +54,58 @@ char * getline(std::istream & in, size_t & s)
   return result;
 }
 
+size_t excsnd(const char * data1, const char * data2, size_t size1, size_t size2)
+{
+  size_t size = 0;
+  for (size_t i = 0; i < size1; i++)
+  {
+    size_t match = 0;
+    for (size_t j = 0; j < size2; j++)
+    {
+      if (data1[i] == data2[j])
+      {
+        match++;
+        break;
+      }
+    }
+    if (match == 0)
+    {
+      size++;
+    }
+  }
+  return size;
+}
+
+char * excsnd(char * res, const char * data1, const char * data2, size_t size1, size_t size2)
+{
+  size_t k = 0;
+  for (size_t i = 0; i < size1; i++)
+  {
+    size_t match = 0;
+    for (size_t j = 0; j < size2; j++)
+    {
+      if (data1[i] == data2[j])
+      {
+        match++;
+        break;
+      }
+    }
+    if (match == 0)
+    {
+      res[k++] = data1[i];
+    }
+  res[k] = 0;
+  }
+  return res;
+}
+
 int main()
 {
-  size_t s = 0;
-  std::cout << getline(std::cin, s) << "\n";
+  size_t size1 = 0;
+  size_t size2 = 0;
+  char * data1 = getline(std::cin, size1);
+  char * data2 = getline(std::cin, size2);
+  size_t size = excsnd(data1, data2, size1, size2);
+  char * res = reinterpret_cast< char * >(malloc(sizeof(char) * size));;
+  std::cout << excsnd(res, data1, data2, size1, size2) << "\n";
 }
