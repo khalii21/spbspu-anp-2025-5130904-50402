e67d3a708fabff2b799874796fc3d9b1b8b8b42d
khalii21 <miratkhalikov007@gmail.com>
2026-02-04 21:48:44 +0500

    getline is done

--- khalikov.mirat/P4/main.cpp
+++ khalikov.mirat/P4/main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 #include <iomanip>
+#include <stdlib.h>
+
+void extend(char ** data, size_t & capacity)
+{
+  size_t new_capacity = capacity * 2;
+  char * new_array = reinterpret_cast< char * >(malloc(sizeof(char) * capacity));;
+  if (new_array == nullptr)
+  {
+    throw std::bad_alloc();
+  }
+  else
+  {
+    for (size_t i = 0; i < capacity; i++)
+    {
+      new_array[i] = (*data)[i];
+    }
+    free(*data);
+    *data = new_array;
+    capacity = new_capacity;
+  }
+}
 
 char * getline(std::istream & in, size_t & s)
 {
@@ -8,15 +29,24 @@ char * getline(std::istream & in, size_t & s)
   {
     in >> std::noskipws;
   }
-
-  s = 10;
+  char ch = 0;
   size_t i = 0;
-  char * result = new char[s];
-  for (; in && i < s - 1; ++i)
+  size_t capacity = 2;
+  char * result = (char*)malloc(capacity * sizeof(char));
+  if (result == nullptr)
   {
-    in >> result[i];
+    throw std::bad_alloc();
+  }
+  while (in >> ch && ch != '\n')
+  {
+    if (i >= capacity-1)
+    {
+      extend(&result, capacity);
+    }
+    result[i++] = ch;
   }
   result[i] = 0;
+  s = i;
   if (is_skipws)
   {
     in >> std::skipws;
@@ -27,5 +57,5 @@ char * getline(std::istream & in, size_t & s)
 int main()
 {
   size_t s = 0;
-  std::cout << getline(std::cin, s);
+  std::cout << getline(std::cin, s) << "\n";
 }
